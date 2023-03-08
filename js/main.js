a1f7317b47e25970f1b2f499fb18051e460aa97d
summerSpark2017 <30925759+summer502@users.noreply.github.com>
2023-03-08 22:41:18 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 // 将footer置于页面最底部
-$(function(){
-    function footerPosition(){
-        $("footer").removeClass("fixed-bottom");
-        var contentHeight = document.body.scrollHeight,
-            winHeight = window.innerHeight;
-        if(!(contentHeight > winHeight)){
-            $("footer").addClass("fixed-bottom");
-        } else {
-            $("footer").removeClass("fixed-bottom");
-        }
-    }
-    footerPosition();
-    $(window).resize(footerPosition);
-});
+// $(function(){
+//     function footerPosition(){
+//         $("footer").removeClass("fixed-bottom");
+//         var contentHeight = document.body.scrollHeight,
+//             winHeight = window.innerHeight;
+//         if(!(contentHeight > winHeight)){
+//             $("footer").addClass("fixed-bottom");
+//         } else {
+//             $("footer").removeClass("fixed-bottom");
+//         }
+//     }
+//     footerPosition();
+//     $(window).resize(footerPosition);
+// });
